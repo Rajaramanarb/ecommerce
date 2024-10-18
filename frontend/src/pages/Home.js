021eea5ee54bfd58ff237d0ca39f0607433b1e18
Rajaramanarb <rajaramanarb@gmail.com>
2024-10-18 11:37:49 +0530

    Intergrating GET products API

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,15 @@
-import {Fragment} from 'react';
+import {Fragment, useEffect, useState} from 'react';
 import ProductCard from '../components/ProductCard';
 
 export default function Home() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch(process.env.REACT_APP_API_URL+'/products')
+        .then(res => res.json())
+        .then(res => setProducts(res))
+    },[])
+
     return <Fragment>        
         <h1 id="products_heading">Latest Products</h1>
 
