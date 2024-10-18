bdf861e5c0191f9ed15fce0ef9c3320a4586cbb7
Rajaramanarb <rajaramanarb@gmail.com>
2024-10-18 16:24:09 +0530

    Creating cart page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import ProductDetail from './pages/ProductDetail';
 import { useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Cart from './pages/Cart';
 
 function App() {
   const [cartItems, setCartItems] = useState([]);
@@ -21,6 +22,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Home />} />
           <Route path="/product/:id" element={<ProductDetail cartItems={cartItems} setCartItems={setCartItems}/>} />
+          <Route path="/cart" element={<Cart cartItems={cartItems} setCartItems={setCartItems}/>} />
         </Routes>
       </div>
       </Router>
