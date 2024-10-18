891af2a4ac22d3760515febb19a2df865fea96b0
Rajaramanarb <rajaramanarb@gmail.com>
2024-10-18 19:23:27 +0530

    Vercel deploy

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -53,40 +53,40 @@ export default function Cart({cartItems, setCartItems}) {
     }
     
     return  cartItems.length > 0  ? <Fragment>
-                <div class="container container-fluid">
-                    <h2 class="mt-5">Your Cart: <b>{cartItems.length} items</b></h2>
+                <div className="container container-fluid">
+                    <h2 className="mt-5">Your Cart: <b>{cartItems.length} items</b></h2>
                     
-                    <div class="row d-flex justify-content-between">
-                        <div class="col-12 col-lg-8">
+                    <div className="row d-flex justify-content-between">
+                        <div className="col-12 col-lg-8">
                             {cartItems.map((item) => 
                             (<Fragment>
                                 <hr />
-                                <div class="cart-item">
-                                    <div class="row">
-                                        <div class="col-4 col-lg-3">
+                                <div className="cart-item">
+                                    <div className="row">
+                                        <div className="col-4 col-lg-3">
                                             <img src={item.product.images[0].image} alt={item.product.name} height="90" width="115" />
                                         </div>
 
-                                        <div class="col-5 col-lg-3">
+                                        <div className="col-5 col-lg-3">
                                             <Link to={"/product/"+item.product._id}>{item.product.name}</Link>
                                         </div>
 
 
-                                        <div class="col-4 col-lg-2 mt-4 mt-lg-0">
+                                        <div className="col-4 col-lg-2 mt-4 mt-lg-0">
                                             <p id="card_item_price">${item.product.price}</p>
                                         </div>
 
-                                        <div class="col-4 col-lg-3 mt-4 mt-lg-0">
-                                            <div class="stockCounter d-inline">
-                                                <span class="btn btn-danger minus" onClick={() => decreaseQty(item)}>-</span>
-                                                <input type="number" class="form-control count d-inline" value={item.qty} readOnly />
+                                        <div className="col-4 col-lg-3 mt-4 mt-lg-0">
+                                            <div className="stockCounter d-inline">
+                                                <span className="btn btn-danger minus" onClick={() => decreaseQty(item)}>-</span>
+                                                <input type="number" className="form-control count d-inline" value={item.qty} readOnly />
 
-                                                <span class="btn btn-primary plus" onClick={() => increaseQty(item)}>+</span>
+                                                <span className="btn btn-primary plus" onClick={() => increaseQty(item)}>+</span>
                                             </div>
                                         </div>
 
-                                        <div class="col-4 col-lg-1 mt-4 mt-lg-0">
-                                            <i id="delete_cart_item" onClick={() => removeItem(item)} class="fa fa-trash btn btn-danger"></i>
+                                        <div className="col-4 col-lg-1 mt-4 mt-lg-0">
+                                            <i id="delete_cart_item" onClick={() => removeItem(item)} className="fa fa-trash btn btn-danger"></i>
                                         </div>
 
                                     </div>
@@ -96,15 +96,15 @@ export default function Cart({cartItems, setCartItems}) {
 
                         </div>
 
-                        <div class="col-12 col-lg-3 my-4">
+                        <div className="col-12 col-lg-3 my-4">
                             <div id="order_summary">
                                 <h4>Order Summary</h4>
                                 <hr />
-                                <p>Subtotal:  <span class="order-summary-values">{cartItems.reduce((acc, item) => (acc + item.qty), 0)} (Units)</span></p>
-                                <p>Est. total: <span class="order-summary-values">${cartItems.reduce((acc, item) => (acc + item.product.price * item.qty), 0)}</span></p>
+                                <p>Subtotal:  <span className="order-summary-values">{cartItems.reduce((acc, item) => (acc + item.qty), 0)} (Units)</span></p>
+                                <p>Est. total: <span className="order-summary-values">${cartItems.reduce((acc, item) => (acc + item.product.price * item.qty), 0)}</span></p>
 
                                 <hr />
-                                <button id="checkout_btn" onClick={placeOrderHandler}class="btn btn-primary btn-block">Place Order</button>
+                                <button id="checkout_btn" onClick={placeOrderHandler}className="btn btn-primary btn-block">Place Order</button>
                             </div>
                         </div>
                     </div>
