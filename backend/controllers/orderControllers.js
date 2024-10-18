e4689149b6546d957a54f50acadc418f2502c879
Rajaramanarb <rajaramanarb@gmail.com>
2024-10-18 10:25:39 +0530

    Building order API

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -1,8 +1,17 @@
-exports.createOrder = (req, res, next) => {
+const orderModel = require('../models/orderModels');
+
+//Create order - /api/v1/order
+exports.createOrder =async (req, res, next) => {
+    const cartItems = req.body;
+    const amount = Number(cartItems.reduce((acc, item) => (acc + item.product.price * item.qty),0)).toFixed(2);
+    const status = 'pending';
+
+    const order = await orderModel.create({cartItems, amount, status});
+    
     res.json (
         {
             success: true,
-            message: "order works!"
+            order
         }
     )
 }
