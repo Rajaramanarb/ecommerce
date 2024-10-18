878d2392ffc6fa74e9e94707cf48d3a91ee05def
Rajaramanarb <rajaramanarb@gmail.com>
2024-10-18 16:08:20 +0530

    Add toast message

--- frontend/src/pages/ProductDetail.js
+++ frontend/src/pages/ProductDetail.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
+import { toast } from 'react-toastify';
 
 export default function ProductDetail({cartItems, setCartItems}) {
     const [product, setProduct] = useState(null);
@@ -17,6 +18,7 @@ export default function ProductDetail({cartItems, setCartItems}) {
         if(!itemExist) {
             const newItem = {product, qty};
             setCartItems((state) => [...state, newItem]);
+            toast.success("cart Item added successfully!");
         }
     }
 
